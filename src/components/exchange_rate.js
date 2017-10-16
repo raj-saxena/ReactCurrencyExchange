7de469f3692bde922f7704d4907fff06250ef900
meghna-saxena <meghna.s1906@gmail.com>
2017-10-16 23:00:59 +0530

    reolving list mapping issue

--- src/components/exchange_rate.js
+++ src/components/exchange_rate.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
@@ -6,18 +7,35 @@ class ExchangeRate extends Component {
   componentDidMount() {
     console.log(this.props.fetchExchangeRate());
 		// this.props.fetchExchangeRate();
-	}
-  render() {
-    // console.log('fetchExchangeRate');
-    return (
-      <div>React Currency Exchange</div>
-    );
   }
+  
+  renderRates() {
+    return _.map(this.props.rates, rate => {
+      return (
+      <li className="list-group-item" key={rate.symbols}>
+        {rate.value}
+      </li>
+    );
+  });
+}
+
+  render() {
+    return (
+      <div>
+        <div>
+          React Currency Exchange
+        </div>
+        <ul className="list-group">
+          {this.renderRates()}
+        </ul>
+      </div>
+    );
+  };
 }
 
 function mapStateToProps(state) {
   return {
-		currency: state.exchangeRates
+		rates: state.exchangeRates
 	}
 }
 
--- src/reducers/reducer_exchange_rate.js
+++ src/reducers/reducer_exchange_rate.js
@@ -1,10 +1,10 @@
-import { FETCH_EXCHANGE } from '../actions';
+import { FETCH_EXCHANGE } from '../actions/type';
 
 export default function(state = {}, action) {
 	switch(action.type) {
         case FETCH_EXCHANGE:
         //returning rates object from payload
-        return [action.payload.rates, ...state];
+        return [action.payload.data.rates, ...state];
 	}
 	return state;
 }
