f35eb713ba12b4d6106c364538ab4fda3b91d964
meghna-saxena <meghna.s1906@gmail.com>
2017-10-17 20:54:32 +0530

    condensing line of codes

--- src/reducers/reducer_exchange_rate.js
+++ src/reducers/reducer_exchange_rate.js
@@ -1,11 +1,9 @@
 import { FETCH_EXCHANGE } from '../actions/type';
 
 export default function(state = {}, action) {
-        // console.log('in reducer');
 	switch(action.type) {
         case FETCH_EXCHANGE:
         //returning rates object from payload
-        // console.log('in FETCH_EXCHANGE');
         return [action.payload.rates, ...state];
 	}
 	return state;
