9b6588860f113bd270ebbf8b3b2bb2ea78226824
Meghna Srivastava <meghna.s1906@gmail.com>
2017-10-22 15:52:35 +0530

    #9 Rendered exchange rates (#11)
    * organised file structure
    
    * payload is returning as promise in console
    
    * modified yarn.lock
    
    * exchange rates rendered on DOM
    
    * made new branch

--- src/components/exchange_rate.js
+++ src/components/exchange_rate.js
@@ -1,23 +1,47 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class ExchangeRate extends Component {
   componentDidMount() {
-    console.log(this.props.fetchExchangeRate());
-		// this.props.fetchExchangeRate();
-	}
-  render() {
-    // console.log('fetchExchangeRate');
-    return (
-      <div>React Currency Exchange</div>
-    );
+    this.props.fetchExchangeRate();
   }
+  
+  renderRates() {
+    const rates = this.props.rates[0];
+      if(!rates) {
+        return 'Fetching data';
+      }
+      
+      console.log('rates', rates.rates);
+      return _.map(rates.rates, (key, value) => {
+        console.log(key + "->" + value);
+        return (
+        <li className="list-group-item" key={key}>
+          {key} {value}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          React Currency Exchange
+        </div>
+        <ul className="list-group">
+          {this.renderRates()}
+        </ul>
+      </div>
+    );
+  };
 }
 
 function mapStateToProps(state) {
   return {
-		currency: state.exchangeRates
+		rates: state.exchangeRates
 	}
 }
 
